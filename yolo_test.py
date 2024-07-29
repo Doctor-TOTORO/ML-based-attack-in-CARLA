88cc90b2fc1b92e3c346d63d083968fa9d34e8e5
Doctor-TOTORO <101552212+Doctor-TOTORO@users.noreply.github.com>
2024-07-29 21:47:07 +0100

    Update yolo_test.py

--- yolo_test.py
+++ yolo_test.py
@@ -19,37 +19,14 @@ if str(ROOT) not in sys.path:
 ROOT = Path(os.path.join(os.path.relpath(ROOT, Path.cwd()), "yolov5"))  # relative
 
 model = yolo.Model(cfg=ROOT / "models/yolov5m.yaml")
-classify_model = yolo.ClassificationModel(cfg=ROOT / "models/yolov5m.yaml", model=model)
+classify_model = yolo.ClassificationModel(cfg=ROOT / "data/coco.yaml", model=model)
 
-
-
-opt = {
-        "weights": str(ROOT / "yolov5s-cls.pt"),
-        "source": str(ROOT),
-        "data": str(ROOT / "data/coco128.yaml"),
-        "imgsz": (224, 224),
-        "device": "",
-        "view_img": False,
-        "save_txt": False,
-        "nosave": False,
-        "augment": False,
-        "visualize": False,
-        "update": False,
-        "project": str(ROOT / "runs/predict-cls"),
-        "name": "exp",
-        "exist_ok": False,
-        "half": False,
-        "dnn": False,
-        "vid_stride": 1
-    }
-model.eval()
 image_path = os.path.join(os.getcwd(), "005286.png")
 image = Image.open(image_path).convert('RGB')
 transform = transforms.ToTensor()
 tensor = transform(image)
 results = classify_model(tensor.unsqueeze(0))
-print(results)
 attacker = SimBA(classify_model, 224)
-_, max_index = torch.max(results, dim=1)
+max_index = torch.argmax(results, dim=1)
+print(max_index)
 fake_image = attacker.simba_single(tensor, max_index)
-print("Done")
