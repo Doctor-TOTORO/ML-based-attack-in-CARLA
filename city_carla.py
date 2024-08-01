ef24bc108ee4ba1f374dd999aa4c545bd2cbbd4e
Doctor-TOTORO <101552212+Doctor-TOTORO@users.noreply.github.com>
2024-08-01 13:56:27 +0100

    Update city_carla.py

--- city_carla.py
+++ city_carla.py
@@ -7,6 +7,7 @@ import os
 import carla
 import time
 import math
+import numpy as np
 
 import opencda.scenario_testing.utils.sim_api as sim_api
 import opencda.scenario_testing.utils.customized_map_api as map_api
@@ -15,6 +16,11 @@ from opencda.scenario_testing.evaluations.evaluate_manager import \
 from opencda.scenario_testing.utils.yaml_utils import \
     add_current_time
 from opencda.core.common.cav_world import CavWorld
+
+file_path = os.path.join(os.getcwd(), 'evaluation_outputs', 'steer_data.npy')
+brake_path = os.path.join(os.getcwd(), 'evaluation_outputs', 'brake_data.npy')
+time_path = os.path.join(os.getcwd(), 'evaluation_outputs', 'time_data.npy')
+distance_path = os.path.join(os.getcwd(), 'evaluation_outputs', 'distance_data.npy')
 
 def get_distance(location1, location2):
     return math.sqrt((location1.x - location2.x)**2 + (location1.y - location2.y)**2 + (location1.z - location2.z)**2)
@@ -63,14 +69,14 @@ def run_scenario(opt, scenario_params):
         walker = None
         blueprint_library = scenario_manager.world.get_blueprint_library()
         walker_bp = blueprint_library.filter('walker.pedestrian.0001')[0]
-        start_location = carla.Location(x=-44, y=118.26, z=1.00)
+        start_location = carla.Location(x=-38.5, y=118.26, z=1.00)
         end_location = carla.Location(x=-56.81, y=118.26, z=1.00)
         direction_vector = carla.Vector3D(x=(end_location.x - start_location.x),
                                           y=(end_location.y - start_location.y),
                                           z=0.00)
         walker_rotation = carla.Rotation(pitch=0.00, yaw=180.00, roll=0.00)
         walker_spawn_point = carla.Transform(location=start_location, rotation=walker_rotation)
-        controller = carla.WalkerControl(direction=direction_vector, speed=0.3)
+        controller = carla.WalkerControl(direction=direction_vector, speed=0.1)
         walker = scenario_manager.world.spawn_actor(walker_bp, walker_spawn_point)
         walker.apply_control(controller)
 
@@ -88,7 +94,10 @@ def run_scenario(opt, scenario_params):
 
 
         spectator = scenario_manager.world.get_spectator()
-
+        steer_data = np.array([])
+        time_data = np.array([])
+        brake_data = np.array([])
+        distance_data = np.array([])
         start_time = time.time()
 
         # run steps
@@ -104,22 +113,22 @@ def run_scenario(opt, scenario_params):
                     carla.Rotation(
                         pitch=-
                         90)))
-            
-            if time.time() - start_time > 7.0:
-                walker.destroy()
-                walker = scenario_manager.world.spawn_actor(walker_bp, walker_spawn_point)
-                walker.apply_control(controller)
-                start_time = time.time()
 
             for i, single_cav in enumerate(single_cav_list):
                 single_cav.update_info()
                 control = single_cav.run_step()
                 single_cav.vehicle.apply_control(control)
-            
-            if transform.location.x >= -70 and transform.location.x <= -65:
-                if transform.location.y >= 25:
-                    break
-                
+                '''
+                if (single_cav.vehicle.get_transform().location.y > 118.26):
+                    steer_data = np.append(steer_data, single_cav.vehicle.get_control().steer)
+                    brake_data = np.append(brake_data, single_cav.vehicle.get_control().brake)
+                    distance_data = np.append(distance_data, get_distance(single_cav.vehicle.get_transform().location, bg_veh_list[3].get_transform().location))
+                    time_data = np.append(time_data, time.time()-start_time)
+                    np.save(file_path, steer_data)
+                    np.save(brake_path, brake_data)
+                    np.save(distance_path, distance_data)
+                    np.save(time_path, time_data)
+                '''
 
     finally:
 
