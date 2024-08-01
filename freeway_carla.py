bb03ec52108e619d10d54085626ba006f6aa35f9
Doctor-TOTORO <101552212+Doctor-TOTORO@users.noreply.github.com>
2024-08-01 14:50:14 +0100

    Update freeway_carla.py

--- freeway_carla.py
+++ freeway_carla.py
@@ -5,6 +5,9 @@ Scenario testing: stop in front of a trafficlight with passengers crossing with 
 
 import os
 import carla
+import time
+import math
+import numpy as np
 
 import opencda.scenario_testing.utils.sim_api as sim_api
 import opencda.scenario_testing.utils.customized_map_api as map_api
@@ -13,6 +16,14 @@ from opencda.scenario_testing.evaluations.evaluate_manager import \
 from opencda.scenario_testing.utils.yaml_utils import \
     add_current_time
 from opencda.core.common.cav_world import CavWorld
+
+file_path = os.path.join(os.getcwd(), 'evaluation_outputs', 'steer_data.npy')
+brake_path = os.path.join(os.getcwd(), 'evaluation_outputs', 'brake_data.npy')
+time_path = os.path.join(os.getcwd(), 'evaluation_outputs', 'time_data.npy')
+distance_path = os.path.join(os.getcwd(), 'evaluation_outputs', 'distance_data.npy')
+
+def get_distance(location1, location2):
+    return math.sqrt((location1.x - location2.x)**2 + (location1.y - location2.y)**2 + (location1.z - location2.z)**2)
 
 
 def run_scenario(opt, scenario_params):
@@ -47,6 +58,11 @@ def run_scenario(opt, scenario_params):
                               current_time=scenario_params['current_time'])
 
         spectator = scenario_manager.world.get_spectator()
+        steer_data = np.array([])
+        time_data = np.array([])
+        brake_data = np.array([])
+        distance_data = np.array([])
+        start_time = time.time()
 
         # run steps
         while True:
@@ -65,6 +81,17 @@ def run_scenario(opt, scenario_params):
                 single_cav.update_info()
                 control = single_cav.run_step()
                 single_cav.vehicle.apply_control(control)
+
+            '''
+            steer_data = np.append(steer_data, single_cav_list[0].vehicle.get_control().steer)
+            brake_data = np.append(brake_data, single_cav_list[0].vehicle.get_control().brake)
+            distance_data = np.append(distance_data, get_distance(single_cav_list[0].vehicle.get_transform().location, single_cav_list[1].vehicle.get_transform().location))
+            time_data = np.append(time_data, time.time()-start_time)
+            np.save(file_path, steer_data)
+            np.save(brake_path, brake_data)
+            np.save(distance_path, distance_data)
+            np.save(time_path, time_data)
+            '''
             
     finally:
         eval_manager.evaluate()
