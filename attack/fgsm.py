86be519792b440bd74e3a383bee273bbb63aed47
Doctor-TOTORO <101552212+Doctor-TOTORO@users.noreply.github.com>
2024-08-01 15:08:12 +0100

    Update fgsm.py

--- attack/fgsm.py
+++ attack/fgsm.py
@@ -2,12 +2,15 @@ import torch
 import torch.nn.functional as F
 
 def fgsm_attack(image, epsilon, data_grad):
+    data_grad = data_grad.cuda()
     sign_data_grad = data_grad.sign()
     perturbed_image = image + epsilon*sign_data_grad
     perturbed_image = torch.clamp(perturbed_image, 0, 1)
     return perturbed_image
 
-def run_attack_cycle(data, target, num_iterations=20, epsilon=0.3):
+def run_attack_cycle(data, target, classify_model, num_iterations=10, epsilon=0.3):
+    data = data.cuda()
+    target = target.cuda()
     for i in range(num_iterations):
         data.requires_grad = True
         output = classify_model(data)
@@ -18,14 +21,9 @@ def run_attack_cycle(data, target, num_iterations=20, epsilon=0.3):
         data_grad = data.grad.data
         perturbed_image = fgsm_attack(data, epsilon, data_grad)
         output = classify_model(perturbed_image)
-        final_pred = output.max(1, keepdim=True)[1]
-        if final_pred.item() != target.item():
-            print("Done")
-        else:
-            print(max_index.item(), final_pred.item())
         data = perturbed_image.detach()
         with torch.no_grad():
             output = classify_model(data)
             target = torch.argmax(output, dim=1)
         if i == num_iterations - 1:
-            return perturbed_image.squeeze()
+            return perturbed_image.squeeze().detach()
