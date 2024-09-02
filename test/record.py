183d3cb2c0a9e06469dc562501977aaf461251ea
Doctor-TOTORO <101552212+Doctor-TOTORO@users.noreply.github.com>
2024-09-02 17:18:23 +0800

    Delete test directory

--- test/record.py
+++
@@ -1,2 +0,0 @@
-def record_time(start_time, end_time):
-    return end_time - start_time
--- test/utils_utils.py
+++
@@ -1,3 +0,0 @@
-def atleast_kdim(x, ndim):
-    shape = x.shape + (1,) * (ndim - len(x.shape))
-    return x.reshape(shape)
